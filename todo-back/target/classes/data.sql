857e615f3e3baa5dce58580b2c3967a7f5840993
IppeW <meddi.fr@gmail.com>
2021-06-22 09:34:30 +0200

    commencement de la modification du projet Todo

--- todo-back/target/classes/data.sql
+++
@@ -1,8 +0,0 @@
-insert into todo(id, username,description,target_date,is_done)
-values(10001, 'meddi', 'Learn JPA', sysdate(), false);
-
-insert into todo(id, username,description,target_date,is_done)
-values(10002, 'meddi', 'Learn Data JPA', sysdate(), false);
-
-insert into todo(id, username,description,target_date,is_done)
-values(10003, 'meddi', 'Learn Microservices', sysdate(), false);
